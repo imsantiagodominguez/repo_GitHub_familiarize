01508e1b1b0df24030a497ac2e150f71ac23e6c0
Santek <santgodominguez@gmail.com>
2026-08-18 13:29:02 -0300

    Create main.py
    `main.py` is created with a greeting from me to the person who enters.

---
+++ main.py
@@ -0,0 +1,3 @@
+print("ESP: Hola soy 'Santek', estoy probando GitHub.\nENG: Hi, im 'Santek', im testing GitHub.")
+nombre = input("Ingrese el nombre de quien esta visitandome!\n")
+print(f"ESP: Bienvenido {nombre} un placer.\nENG: Welcome {nombre} its a pleasure.")
